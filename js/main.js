34002e47ecfd63d176ffa34ff3f669fda1725f08
ElisaME <elisam.esquivel@gmail.com>
2016-05-30 13:41:44 -0500

    Menu funciona

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	var $hamburger = $(".hamburger");
+	$hamburger.on("click", function(e) {
+		 $hamburger.toggleClass("is-active");
+		// Do something else, like open/close menu
+		$('.opciones-menu').toggle();
+	});
+});
+	
+	
